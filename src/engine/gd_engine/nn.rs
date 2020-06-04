40f5b089810e65c1115e2a2f02e8a03452855bb8
Manuel Drehwald <manuel@drehwald.info>
2020-06-04 05:11:15 +0200

    wip Neural network

--- src/engine/gd_engine/nn.rs
+++ src/engine/gd_engine/nn.rs
@@ -1,3 +1,8 @@
+use ndarray::Array;
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::normal::StandardNormal;
+
+
 pub struct NeuralNetwork {
   H: usize,
   batch_size: usize,
@@ -11,8 +16,12 @@ pub struct NeuralNetwork {
 
 impl NeuralNetwork {
   pub fn new(input_dim: usize) -> Neuralnetwork {
+    //xavier init
+    let NN = Array::random((36,36), StandardNormal::new())
+      .map(|&x| x / sqrt(36));
+
     NeuralNetwork{
-      H: 200,
+      H: 36,//200,
       D: input_dim,
       batch_size = 10,
       learning_rate = 1e-4,
@@ -24,52 +33,33 @@ impl NeuralNetwork {
 
   pub fn init() {
   }
-
 }
+
+fn softmax(x: Vec<f32>) -> Vec<f32> {
+  max = 
+}
+
+fn policy_forward(x) -> Vec<f32> {
+  let output = W1.dot(x);
+    
+    //.map(|&x| 1 / (1 + (-x).exp())) //sigmoid
+    //.map(|&x| if x < 0 { 0 } else { x }); //ReLu for multilayer
+}
+
+fn discount_rewards(moves_prob, legal_moves: Vec<i32>) -> f32 {
+  let n = legal_moves.iter().sum() as f32;
+  let normalized_legal_moves: Vec<f32> = legal_moves.iter().map(|&x| (x as f32)/num_legal_moves).collect();
+  //MSE pushed the neural net into giving all allowed moves the same prob and 0 to the rest
+  //+1 for mimicing legal moves perfectly, neg values if it is too far
+  let MSE = moves_prob.iter().
+    zip(normalized_legal_moves.iter())
+    .fold(0.0, |sum, (&x, &y)| sum + ((x-y)*3.0).powf(2.0));
+  1 - MSE
+}
+
+fn policy_backward(
+
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
-
-
-# model initialization
-D = 80 * 80 # input dimensionality: 80x80 grid
-if resume:
-  model = pickle.load(open('save.p', 'rb'))
-else:
-  model = {}
-  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
-  model['W2'] = np.random.randn(H) / np.sqrt(H)
-  
-grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
-
-def sigmoid(x): 
-  return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
-
-def prepro(I):
-  """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  I = I[35:195] # crop
-  I = I[::2,::2,0] # downsample by factor of 2
-  I[I == 144] = 0 # erase background (background type 1)
-  I[I == 109] = 0 # erase background (background type 2)
-  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel()
-
-def discount_rewards(r):
-  """ take 1D float array of rewards and compute discounted reward """
-  discounted_r = np.zeros_like(r)
-  running_add = 0
-  for t in reversed(xrange(0, r.size)):
-    if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
-    running_add = running_add * gamma + r[t]
-    discounted_r[t] = running_add
-  return discounted_r
-
-def policy_forward(x):
-  h = np.dot(model['W1'], x)
-  h[h<0] = 0 # ReLU nonlinearity
-  logp = np.dot(model['W2'], h)
-  p = sigmoid(logp)
-  return p, h # return probability of taking action 2, and hidden state
-
 def policy_backward(eph, epdlogp):
   """ backward pass. (eph is array of intermediate hidden states) """
   dW2 = np.dot(eph.T, epdlogp).ravel()
@@ -77,6 +67,14 @@ def policy_backward(eph, epdlogp):
   dh[eph <= 0] = 0 # backpro prelu
   dW1 = np.dot(dh.T, epx)
   return {'W1':dW1, 'W2':dW2}
+
+  
+grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
+rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
+
+
+
+
 
 env = gym.make("Pong-v0")
 observation = env.reset()
