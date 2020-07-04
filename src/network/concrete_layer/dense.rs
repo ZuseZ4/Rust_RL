8b7fa0895310fea2fd41866777d56fce9f69f805
Manuel Drehwald <manuel@drehwald.info>
2020-07-05 01:34:00 +0200

    cleaning up

--- src/network/concrete_layer/dense.rs
+++ src/network/concrete_layer/dense.rs
@@ -16,18 +16,18 @@ pub struct DenseLayer {
 }
 
 impl DenseLayer {
-  pub fn new(input_dim: usize, output_dim: usize, batch_size: usize, l_r: f32) -> Self {
+  pub fn new(input_dim: usize, output_dim: usize, batch_size: usize, learning_rate: f32) -> Self {
     //xavier init
     //let nn_weights: Array2<f32> = Array::random((input_dim,output_dim), Normal::new(0.0, 1.0).unwrap()) 
-    let nn_weights: Array2<f32> = Array::random((output_dim,input_dim), Normal::new(0.0, 1.0/((output_dim+input_dim) as f32/2.0)).unwrap()) 
+    let weights: Array2<f32> = Array::random((output_dim,input_dim), Normal::new(0.0, 1.0/((output_dim+input_dim) as f32/2.0)).unwrap()) 
       .map(|&x| x / (input_dim as f32).sqrt());
-    let nn_bias: Array1<f32> = Array::zeros(output_dim);//https://cs231n.github.io/neural-networks-2/#init
+    let bias: Array1<f32> = Array::zeros(output_dim);//https://cs231n.github.io/neural-networks-2/#init
     DenseLayer{
       input_dim,
       output_dim,
-      learning_rate: l_r,
-      weights: nn_weights,
-      bias: nn_bias,
+      learning_rate,
+      weights,
+      bias,
       net: Array::zeros((input_dim, batch_size)),
       feedback: Array::zeros((output_dim, batch_size)),
       batch_size,
@@ -47,10 +47,10 @@ impl Layer for DenseLayer {
   fn forward(&mut self, x: Array1<f32>) -> Array1<f32> {
     let pos_in_batch = self.predictions % self.batch_size;
     self.net.column_mut(pos_in_batch).assign(&x); 
-    //println!("before {:?}: {:?}   {:?}: {:?}", self.weights.shape(), self.weights, last_net.shape(), last_net);
     let res: Array1<f32> = self.weights.dot(&x) + &self.bias; //was this
     res
   }
+
 
   fn backward(&mut self, feedback: Array1<f32>) -> Array1<f32> {
     
@@ -59,30 +59,20 @@ impl Layer for DenseLayer {
 
     //calc derivate to backprop through layers
     let output = self.weights.t().dot(&feedback.t());
-    //println!("backprop dense feedback {:?} {:?}",output.shape(), output);
-    //println!("weights: {:?}", self.weights);
-
 
 
     //store feedback
     self.predictions += 1;
     if self.predictions % self.batch_size == 0 {
-      //let d_w: Array2<f32> = &self.feedback.t().dot(&self.net) * self.learning_rate / (self.batch_size as f32);
-      let d_w: Array2<f32> = &self.feedback.dot(&self.net.t()) * self.learning_rate / (self.batch_size as f32);//before
+      let d_w: Array3<f32> = &self.feedback.dot(&self.net.t()) * self.learning_rate / (self.batch_size as f32);
       let d_b: Array1<f32> = &self.feedback.sum_axis(Axis(1))  * self.learning_rate / (self.batch_size as f32); 
       
-      //println!("weight update sum: {}", d_w.fold(0.0, |sum, val| sum+val));
-      //println!("bias update sum: {}", d_b.fold(0.0, |sum, val| sum+val));
       
       self.weights -= &d_w;
       self.bias    -= &d_b;
-    
+   
+
       //self.learning_rate *= 0.999;
-      //println!("new learning rate: {}",self.learning_rate);
-      //println!("weights sum: {}", self.weights.fold(0.0, |sum, val| sum+val));
-      //println!("bias sum: {}\n", self.bias.fold(0.0, |sum, val| sum+val));
-      //println!("weights {}", self.weights);
-      //println!("bias {}\n", self.bias);
       self.net = Array::zeros((self.input_dim, self.batch_size)); //can be skipped, just ignore/overwrite old vals
       self.feedback = Array::zeros((self.output_dim, self.batch_size)); //can be skipped, just ignore/overwrite old vals
     }
